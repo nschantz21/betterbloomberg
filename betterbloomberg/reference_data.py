da884848f4f3083566e581c076507d756dbb063a
nschantz21 <nschantz21@gmail.com>
2018-09-01 00:39:59 -0400

    added more to he reference data class

--- betterbloomberg/reference_data.py
+++ betterbloomberg/reference_data.py
@@ -1,16 +1,15 @@
 
 """
-Limitations exist on the number of fields for reference and historical data request: 400 fields for reference data request and 25 fields for historical data request. There is also a limit on the number of securities enforced by the Session’s MaxPendingRequests. API will split the securities in the request into groups of 10 securities and fields into groups of 128 fields. Therefore, depending of the number of securities and fields provided, the number of requests many exceed the default 1,024 MaxPendingRequests limit.
+Limitations exist on the number of fields for reference and historical data request:  400 fields for reference data request and 25 fields for historical data request. There is also a limit on the number of securities enforced by the Session's MaxPendingRequests. API will split the securities in the request into groups of 10 securities and fields into groups of 128 fields. Therefore, depending of the number of securities and fields provided, the number of requests many exceed the default 1,024 MaxPendingRequests limit.
 """
-
-
-import blpapi
+import blpapi # I think you can import this at the module level
+import pandas as pd
 from abc import ABCMeta, abstractmethod, abstractproperty
 
-    
-# I think requires a class hierarchy
+
 class BlpDataRequest(object):
     __metaclass__ = ABCMeta
+    
     def __init__(self, host='localhost', port=8194, service_type=None, request_type=None, **kwargs):
         self.host = host
         self.port = port
@@ -22,6 +21,13 @@ class BlpDataRequest(object):
         self.session = BlpDataRequest.session_handle(self.host, self.port)
         self.service = BlpDataRequest.service_handle(self.session, self.service_type)
         self.request = self.service.createRequest(self.request_type)
+        # not sure if these should be included at this level
+        self.generate_request()
+        self.send_request()
+        self.data = self.process_response()
+        self.session.stop()
+    
+    # add extra logic to the the destructor to close the service
        
     @abstractproperty
     def service_type(self):
@@ -35,6 +41,7 @@ class BlpDataRequest(object):
     def generate_request(self):
         pass
     
+    # I don't think this needs to be an abstract method
     def send_request(self, correlation_id = None):
         eQ = blpapi.event.EventQueue()
         if correlation_id is not None:
@@ -45,11 +52,11 @@ class BlpDataRequest(object):
             if eventObj.eventType() == blpapi.event.Event.RESPONSE:
                 # A RESPONSE Message indicates the request has been fully served
                 break
-        return eventObj
+        self.response = eventObj
 
     
     @abstractmethod
-    def process_request(self):
+    def process_response(self):
         pass
     
     @staticmethod
@@ -69,6 +76,7 @@ class BlpDataRequest(object):
         if (not sess.openService(service_type)):
             print "failed to open {0} service".format(service_type)
         return sess.getService(service_type)
+        
 
 
 class StaticReferenceData(BlpDataRequest):
@@ -82,20 +90,64 @@ class StaticReferenceData(BlpDataRequest):
 class ReferenceDataRequest(StaticReferenceData):
     request_type = "ReferenceDataRequest"
     
-    def __init__(self, securities, fields, overrides, **kwargs):
+    def __init__(self, securities, fields, overrides=None, **kwargs):
+        """
+        securities : array-like
+            security identifiers
+        fields : array-like
+            reference fields
+        overrides : dict
+            override fields and values
+        """
         self.securities = securities
         self.fields = fields
-        self.overrides = overrides
+        if overrides is not None:
+            self.overrides = overrides
         super(ReferenceDataRequest, self).__init__(**kwargs)
-        
+        # self.session
+        # self.service
+        # self.request
+        # self.response
     
     def generate_request(self):
         # constructing the request
-        print "gen"
-        
+        for s in self.securities:
+            self.request.append("securities", s)
+        for f in self.fields:
+            self.request.append("fields", f)
+        # setting the overrides is a bitch.
+        # might be a better way to do this
+        ovrds = self.request.getElement("overrides")
+        for (k, v) in self.overrides.iteritems():
+            ovr = ovrds.appendElement()
+            ovr.setElement("fieldId", k)
+            ovr.setElement("value", v)
     
-    def process_request(self):
-        print "proc"
+    def processBulkField(refBulkfield):
+        # get the total number of Bulk data points
+        # get the number of subfields for each bulk data element
+        # read each field in bulk data
         
+        pass
+    
+    
+    
+    def process_response(self):
+        SECURITY_DATA = blpapi.Name("fieldDate")
+        FIELD_DATA = blpapi.Name("fieldInfo")
+        securityData = blp.event.MessageIterator(self.response).next.getElement(SECURITY_DATA)
+        my_dict = dict()
+        for i in xrange(securityData.numValues()):
+            tmp_sec = securityData.getValueAsElement(i)
+            fid = tmp_sec.getElementAsString("id")
+            my_dict[fid] = dict()
+            for f in ['mnemonic', 'description', 'documentation']:
+                my_dict[fid][f] = tmp_sec.getElement(FIELD_DATA).getElementAsString(f)
+            if overrides:
+                ovrd_list = list()
+                for j in xrange(tmp_sec.getElement(FIELD_DATA).getElement('overrides').numValues()):
+                    ovrd_list.append(tmp_sec.getElement(FIELD_DATA).getElement('overrides').getValue(j))
+                my_dict[fid]['overrides'] = ovrd_list 
+        return pd.DataFrame.from_dict(my_dict, orient='index')
 
     
