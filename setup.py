7fa53abc5210f8f96e519b066b3f6745b880bccb
nschantz21 <nschantz21@gmail.com>
2020-05-12 23:18:07 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name="betterbloomberg",
-    version="0.1.0",
-    description="A simple wrapper to the Bloomberg Python API",
+    version="0.0.2",
+    description="A simple wrapper for the Bloomberg Python API",
     url="https://github.com/nschantz21/betterbloomberg",
     author="Nick Schantz",
     author_email="nschantz21@gmail.com",
--- tests/all_test.py
+++
@@ -1,9 +0,0 @@
-from betterbloomberg import get
-
-data = get(
-    kind="ReferenceDataRequest",
-    securities=["AAPL US EQUITY",],
-    fields=["PX_LAST",]
-)
-
-print(data)
