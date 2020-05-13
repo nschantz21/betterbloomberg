4accdcc03e02d9f0e52a77703455fd2ab63e3929
nschantz21 <nschantz21@gmail.com>
2020-05-12 23:11:04 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from distutils.core import setup
 
 setup(
     name="betterbloomberg",
-    version="0.0.1",
+    version="0.1.0",
+    description="A simple wrapper to the Bloomberg Python API",
+    url="https://github.com/nschantz21/betterbloomberg",
+    author="Nick Schantz",
+    author_email="nschantz21@gmail.com",
+    license="GNU",
     packages=["betterbloomberg",]
 )
